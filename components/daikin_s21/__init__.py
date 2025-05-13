4882794759a74f16cc959b99e766d30e39e0ce7c
hoangtridung <hoangtridung@gmail.com>
2025-05-13 12:06:34 +0700

    Update __init__.py

--- components/daikin_s21/__init__.py
+++ components/daikin_s21/__init__.py
@@ -1,61 +1,45 @@
+"""
+Daikin S21 Mini-Split ESPHome component config validation & code generation.
+"""
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, uart
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['uart']
+DEPENDENCIES = ["uart"]
+
+CONF_TX_UART = "tx_uart"
+CONF_RX_UART = "rx_uart"
+CONF_S21_ID = "s21_id"
+CONF_DEBUG_PROTOCOL = "debug_protocol"
 
 daikin_s21_ns = cg.esphome_ns.namespace("daikin_s21")
-DaikinS21 = daikin_s21_ns.class_("DaikinS21", cg.Component)
+DaikinS21 = daikin_s21_ns.class_("DaikinS21", cg.PollingComponent)
+DaikinS21Client = daikin_s21_ns.class_("DaikinS21Client")
+uart_ns = cg.esphome_ns.namespace("uart")
+UARTComponent = uart_ns.class_("UARTComponent")
 
-# Sensor schema
-SENSOR_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(sensor.Sensor),
-}).extend(sensor.sensor_schema(
-    accuracy_decimals=1,
-    device_class="temperature",
-    state_class="measurement",
-    unit_of_measurement="°C",
-))
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(DaikinS21),
+        cv.Required(CONF_TX_UART): cv.use_id(UARTComponent),
+        cv.Required(CONF_RX_UART): cv.use_id(UARTComponent),
+        cv.Optional(CONF_DEBUG_PROTOCOL, default=False): cv.boolean,
+    }
+).extend(cv.polling_component_schema("2s"))
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(DaikinS21),
-    cv.Required("tx_uart"): cv.use_id(uart.UARTComponent),
-    cv.Required("rx_uart"): cv.use_id(uart.UARTComponent),
-    cv.Optional("sensor"): cv.Schema({
-        cv.Required("s21_id"): cv.use_id(DaikinS21),
-        cv.Optional("inside_temperature"): SENSOR_SCHEMA,
-        cv.Optional("outside_temperature"): SENSOR_SCHEMA,
-        cv.Optional("fan_speed"): cv.Schema({
-            cv.GenerateID(): cv.declare_id(sensor.Sensor),
-        }).extend(sensor.sensor_schema(
-            accuracy_decimals=0,
-            device_class="speed",
-            state_class="measurement",
-            unit_of_measurement="rpm",
-        )),
-    }),
-}).extend(cv.COMPONENT_SCHEMA)
+S21_CLIENT_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_S21_ID): cv.use_id(DaikinS21),
+    }
+)
+
 
 async def to_code(config):
+    """Generate code"""
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    tx_uart_ = await cg.get_variable(config["tx_uart"])
-    rx_uart_ = await cg.get_variable(config["rx_uart"])
-    cg.add(var.set_tx_uart(tx_uart_))
-    cg.add(var.set_rx_uart(rx_uart_))
-
-    if "sensor" in config:
-        s21_ = await cg.get_variable(config["sensor"]["s21_id"])
-        if "inside_temperature" in config["sensor"]:
-            temp = cg.new_Pvariable(config["sensor"]["inside_temperature"][CONF_ID])
-            await sensor.register_sensor(temp, config["sensor"]["inside_temperature"])
-            cg.add(s21_.set_inside_temperature_sensor(temp))
-        if "outside_temperature" in config["sensor"]:
-            outside = cg.new_Pvariable(config["sensor"]["outside_temperature"][CONF_ID])
-            await sensor.register_sensor(outside, config["sensor"]["outside_temperature"])
-            cg.add(s21_.set_outside_temperature_sensor(outside))
-        if "fan_speed" in config["sensor"]:
-            fan = cg.new_Pvariable(config["sensor"]["fan_speed"][CONF_ID])
-            await sensor.register_sensor(fan, config["sensor"]["fan_speed"])
-            cg.add(s21_.set_fan_speed_sensor(fan))
+    tx_uart = await cg.get_variable(config[CONF_TX_UART])
+    rx_uart = await cg.get_variable(config[CONF_RX_UART])
+    cg.add(var.set_uarts(tx_uart, rx_uart))
+    cg.add(var.set_debug_protocol(config[CONF_DEBUG_PROTOCOL]))
